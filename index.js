cb9b3af3c4f12f264e30629a0b9252f0aec21e9d
Yuting Zhang <yutingzhang729@gmail.com>
2019-02-08 21:53:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 function theBeatlesPlay(musicians, instruments){
   var solution = [];
   for(var i =0; i<musicians.length; i++){
-    var string = musicians[0]+" plays the "+instruments[0];
+    var string = musicians[i]+" plays the "+instruments[i];
     solution.push(string);
   }
   return solution;
+}
+
+function johnLennonFacts(facts){
+  for(var i = 0; i<facts.length; i++){
+    facts[i] = facts[i]+"!!!";
+  }
+  return facts;
 }
