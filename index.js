a707bdefe651ca0a26715804193769567ea66b4f
Yuting Zhang <yutingzhang729@gmail.com>
2019-02-08 21:52:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var solution = [];
+  for(var i =0; i<musicians.length; i++){
+    var string = musicians[0]+" plays the "+instruments[0];
+    solution.push(string);
+  }
+  return solution;
+}
